0f8ca3b75443f5d56832b0b795e93eac94251e8b
FBGOD2 <s.vxncxd.s@gmail.com>
2023-10-08 19:02:13 +0300

    added new languages

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  print('Hello World');
+  var lang := ReadString('Введите язык:');
+  case lang of
+    'Русский': print('Привет');
+    'Deutsch': print('Hallo');
+    'English': print('Hello');
+    '日本語': print('こんにちは');
+    'Español': print('Hola');
+    else print('Error');
+  end;
 end.
